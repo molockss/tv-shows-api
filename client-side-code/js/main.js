019e3c34d4ebff75e00d755b0f8ec5cc5cd4decf
molockss <mahamed_67@hotmail.com>
2022-06-18 14:41:36 +0100

    api working

--- client-side-code/js/main.js
+++ client-side-code/js/main.js
@@ -3,11 +3,11 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const series = document.querySelector('input').value
     try{
-        const response = await fetch(`https://rap-names-api-fun.herokuapp.com/api/${rapperName}`)
+        const response = await fetch(`https://best-tv-showss-api.herokuapp.com/api/${series}`)
         const data = await response.json()
 
         console.log(data)
-        document.querySelector('h2').innerText = data.birthName
+        document.querySelector('h2').innerText = data.Description
     }catch(error){
         console.log(error)
     }
